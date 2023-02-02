9d91d7edc2f4361c5dede1680942e7996b3ff58b
Skultty <samuel.skultety@outlook.sk>
2023-02-02 18:12:26 +0100

    player factory and game module added

--- main.js
+++ main.js
@@ -18,3 +18,30 @@ const displayController = (() => {
     return { render };
 })();
 
+const player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return { getName, getSymbol };
+};
+
+const game = (() => {
+    const player1 = player("Player 1", "X");
+    const player2 = player("Player 2", "O");
+    const board = gameboard.getBoard();
+    const squares = document.querySelectorAll('.square');
+    const render = () => displayController.render();
+    const start = () => {
+        render();
+        squares.forEach((square, index) => {
+            square.addEventListener('click', () => {
+                if (board[index] === "") {
+                    gameboard.setBoard(index, player1.getSymbol());
+                    render();
+                } 
+            });
+        });
+    };
+    return { start };
+})();
+
+game.start();
